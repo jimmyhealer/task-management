6e220cc6e976a8c2835ed6cbdc8717991068facf
jimmyhealer <yahing6066@gmail.com>
2023-04-02 16:43:45 +0800

    chore: set up prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabwidth: 2,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  printWidth: 80,
+  proseWrap: 'preserve',
+  endOfLine: 'auto',
+}
